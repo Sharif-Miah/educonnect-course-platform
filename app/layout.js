6af672eae23d59443d6ca7e6db375bd6ea3dab70
Sharif-Miah <farhansharif141077@gmail.com>
2026-08-21 11:37:47 +0600

    feat: initialize root layout with global styles, database connection, and favicon assets

--- app/layout.js
+++ app/layout.js
@@ -7,26 +7,26 @@ const inter = Inter({ subsets: ["latin"] });
 const poppins = Inter({ subsets: ["latin"], variable: "--font-poppins" });
 
 export const metadata = {
-  title: "EduConnect - World's Best Learning Platform",
+  title: "EduPlus - World's Best Learning Platform",
   description: "Explore || Learn || Build || Share",
-  icons: {
-    icon: [
-      { url: "/icon.svg", type: "image/svg+xml" },
-      { url: "/logo.svg", type: "image/svg+xml" },
-    ],
-    shortcut: "/icon.svg",
-    apple: "/icon.svg",
-  },
+  // icons: {
+  //   icon: [
+  //     { url: "/icon.svg", type: "image/svg+xml" },
+  //     { url: "/logo.svg", type: "image/svg+xml" },
+  //   ],
+  //   shortcut: "/icon.svg",
+  //   apple: "/icon.svg",
+  // },
 };
 
 export default async function RootLayout({ children }) {
-  const conn  = await dbConnect();
+  const conn = await dbConnect();
   return (
     <html lang="en">
       <body
         className={cn(inter.className, poppins.className)}>
-          {children}
-          <Toaster richColors position="top-center"/>
+        {children}
+        <Toaster richColors position="top-center" />
       </body>
     </html>
   );
